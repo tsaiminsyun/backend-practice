7f9674dcc88f500c5a286088ed1affab8a05ad75
Jerry Tsai <jerry_tsai@cyberinfo.com.tw>
2026-09-11 11:32:45 +0800

    create a todo list

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import express from "express";
+import { json } from "stream/consumers";
 
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+
+type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+const todos: Todo[] = [];
+let nextId = 1;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -9,6 +21,36 @@ app.get("/", (req, res) => {
 
 app.get("/health", (req, res) => {
   res.json({ state: "ok" });
+});
+
+app.get("/todos", (req, res) => {
+  res.json({
+    todos,
+  });
+});
+
+app.post("/todos", (req, res) => {
+  const { title } = req.body;
+
+  if (!title) {
+    res.status(400).json({
+      message: "title is required",
+    });
+    return;
+  }
+
+  const todo: Todo = {
+    id: nextId,
+    title,
+    completed: false,
+  };
+
+  todos.push(todo);
+  nextId += 1;
+
+  res.status(201).json({
+    data: todo,
+  });
 });
 
 app.listen(port, () => {
